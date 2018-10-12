afdd91ef8ab9f1cbe19343a2a7e5c8501d332efd
Max <ermakov.1828@gmail.com>
2018-10-13 01:31:55 +0300

    algorithms ready

--- lab2/test.cpp
+++ lab2/test.cpp
@@ -58,42 +58,85 @@ matrix vinogradMultiply(const matrix& m1, const matrix& m2, int a, int b, int c,
         }
         result.push_back(tmp);
     }
-
     std::vector<int> rowFactor;
     for (int i = 0; i < a; i++) {
         rowFactor.push_back(0);
     }
     for (int i = 0; i < a; i++) {
-        // rowFactor[i] = m1[i][1] * m1[i][2];
         for (int j = 0; j < b/2; j++) {
             rowFactor[i] += m1[i][2*j] * m1[i][2*j+1];
         }
     }
-
     std::vector<int> columnFactor;
     for (int i = 0; i < a; i++) {
         columnFactor.push_back(0);
     }
-
     for (int i = 0; i < c; i++) {
-        // columnFactor[i] = m2[1][i] * m2[2][i];
         for (int j = 0; j < b/2; j++) {
             columnFactor[i] += m2[2*j][i] * m2[2*j+1][i];
         }
     }
-
     for (int i = 0; i < a; i++) {
         for (int j = 0; j < c; j++) {
             result[i][j] = -rowFactor[i] - columnFactor[j];
             for (int k = 0; k < b/2; k++) {
-                // result[i][j] += ( m1[i][2*k-1] + m2[2*k][j]) * 
-                //                     (m1[i][2*k] + m2[2*k-1][j]);
                 result[i][j] += ( m1[i][2*k] + m2[2*k + 1][j]) * 
                                     (m1[i][2*k + 1] + m2[2*k][j]);
             }
         }
     }
+    if (b % 2) {
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < c; j++) {
+                result[i][j] += m1[i][b-1] * m2[b-1][j];
+            }
+        }
+    }
+    return result;
+}
 
+matrix betterVinogradMultiply(const matrix& m1, const matrix& m2, int a, int b, int c, int d) {
+    matrix result;
+    int index;
+    for (int i = 0; i < a; i++){
+        std::vector<int> tmp;
+        for (int j = 0; j < c; j++){
+            tmp.push_back(0);
+        }
+        result.push_back(tmp);
+    }
+
+    int half = b/2; // оптимизация 1
+    std::vector<int> rowFactor;
+    for (int i = 0; i < a; i++) {
+        rowFactor.push_back(0);
+    }
+    for (int i = 0; i < a; i++) {
+        for (int j = 0; j < half; j++) {
+            index = j << 1; // оптимизация 2
+            rowFactor[i] += m1[i][index] * m1[i][index+1]; // оптимизация 4 (+=)
+        }
+    }
+    std::vector<int> columnFactor;
+    for (int i = 0; i < a; i++) {
+        columnFactor.push_back(0);
+    }
+    for (int i = 0; i < c; i++) {
+        for (int j = 0; j < half; j++) {
+            index = j << 1; // оптимищация 3 (вместо 2 умножений будет один побитовый сдвиг)
+            columnFactor[i] += m2[index][i] * m2[index+1][i];
+        }
+    }
+    for (int i = 0; i < a; i++) {
+        for (int j = 0; j < c; j++) {
+            result[i][j] = -rowFactor[i] - columnFactor[j];
+            for (int k = 0; k < half; k++) {
+                index = k << 1;
+                result[i][j] += ( m1[i][index] + m2[index + 1][j]) * 
+                                    (m1[i][index + 1] + m2[index][j]);
+            }
+        }
+    }
     if (b % 2) {
         for (int i = 0; i < a; i++) {
             for (int j = 0; j < c; j++) {
@@ -105,12 +148,14 @@ matrix vinogradMultiply(const matrix& m1, const matrix& m2, int a, int b, int c,
 }
 
 
+
 int main(int argc, char const *argv[])
 {   
     matrix m1; 
     matrix m2;
     matrix rCommon;
     matrix rVin;
+    matrix better_rVin;
     matrixGen(3,3,m1);
     std::cout << "\nfirst matrix" << "\n";
     printMatrix(m1);
@@ -128,6 +173,12 @@ int main(int argc, char const *argv[])
 
     std::cout << "\nVinograd matrix" << "\n";
     printMatrix(rVin);
+
+    better_rVin = betterVinogradMultiply(m1,m2,3,3,3,3);
+
+    std::cout << "\nBetter Vinograd matrix" << "\n";
+    printMatrix(better_rVin);
+
 
     return 0;
 }
