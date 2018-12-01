535a50ad83386feb5c75cd0618876a847d81bbaf
Max <ermakov.1828@gmail.com>
2018-12-01 22:13:10 +0300

    passed 1 module

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -57,78 +57,20 @@ void shellSort(std::vector<int> &arr, int n){
 
 
 
-// Merges two subarrays of arr[]. 
-// First subarray is arr[l..m] 
-// Second subarray is arr[m+1..r] 
-void merge(std::vector<int> &arr, int l, int m, int r) 
+void insertionSort(std::vector<int> arr, int n) 
 { 
-    int i, j, k; 
-    int n1 = m - l + 1; 
-    int n2 =  r - m; 
-  
-    /* create temp arrays */
-    int L[n1], R[n2]; 
-  
-    /* Copy data to temp arrays L[] and R[] */
-    for (i = 0; i < n1; i++) 
-        L[i] = arr[l + i]; 
-    for (j = 0; j < n2; j++) 
-        R[j] = arr[m + 1+ j]; 
-  
-    /* Merge the temp arrays back into arr[l..r]*/
-    i = 0; // Initial index of first subarray 
-    j = 0; // Initial index of second subarray 
-    k = l; // Initial index of merged subarray 
-    while (i < n1 && j < n2) 
-    { 
-        if (L[i] <= R[j]) 
-        { 
-            arr[k] = L[i]; 
-            i++; 
-        } 
-        else
-        { 
-            arr[k] = R[j]; 
-            j++; 
-        } 
-        k++; 
-    } 
-  
-    /* Copy the remaining elements of L[], if there 
-       are any */
-    while (i < n1) 
-    { 
-        arr[k] = L[i]; 
-        i++; 
-        k++; 
-    } 
-  
-    /* Copy the remaining elements of R[], if there 
-       are any */
-    while (j < n2) 
-    { 
-        arr[k] = R[j]; 
-        j++; 
-        k++; 
-    } 
-} 
-  
-/* l is for left index and r is right index of the 
-   sub-array of arr to be sorted */
-void mergeSort(std::vector<int> &arr, int l, int r) 
-{ 
-    if (l < r) 
-    { 
-        // Same as (l+r)/2, but avoids overflow for 
-        // large l and h 
-        int m = l+(r-l)/2; 
-  
-        // Sort first and second halves 
-        mergeSort(arr, l, m); 
-        mergeSort(arr, m+1, r); 
-  
-        merge(arr, l, m, r); 
-    } 
+   int i, key, j; 
+   for (i = 1; i < n; i++) 
+   { 
+       key = arr[i]; 
+       j = i-1; 
+       while (j >= 0 && arr[j] > key) 
+       { 
+           arr[j+1] = arr[j]; 
+           j = j-1; 
+       } 
+       arr[j+1] = key; 
+   } 
 } 
 
 
@@ -240,22 +182,22 @@ int main() {
         v3 = randomSort(k);
 
         tb1 = clock();
-        mergeSort(v1, 0, v1.size() - 1); 
+        insertionSort(v1, v1.size()); 
         te1 = clock();
         result1 = te1 - tb1;
-        std::cout << "MERGE UP " << k << "time: " << result1 << "\n";
+        std::cout << "insertionSort UP " << k << "time: " << result1 << "\n";
 
         tb2 = clock();
-        mergeSort(v2, 0, v2.size() - 1);
+        insertionSort(v2, v2.size());
         te2 = clock();
         result2 = te2 - tb2;
-        std::cout << "MERGE DOWN " << k << "time: " << result2 << "\n";
+        std::cout << "insertionSort DOWN " << k << "time: " << result2 << "\n";
 
         tb3 = clock();
-        mergeSort(v3, 0, v3.size() - 1);
+        insertionSort(v3, v3.size());
         te3 = clock();
         result3 = te3 - tb3;
-        std::cout << "MERGE RANDOM " << k << "time: " << result3 << "\n";
+        std::cout << "insertionSort RANDOM " << k << "time: " << result3 << "\n";
 
         std::cout << "\n";
 
